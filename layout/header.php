f631a8c9b923998b1eb389a1118b586fa1afbdc1
Ramesh Ghosh <star.ramesh.official@gmail.com>
2026-09-17 12:57:08 +0530

    add

---
+++ layout/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <meta name="description" content="Billing Portal">
+    <meta name="author" content="">
+
+    <title>
+        <?= isset($pageTitle) ? htmlspecialchars($pageTitle) : 'Billing Portal' ?>
+    </title>
+
+    <!-- Font Awesome -->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+
+    <!-- Google Font -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Main CSS -->
+    <link href="css/main.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+    <div id="wrapper">
